1d90fe626a5fa77234848ea3d653c0714e3b3869
Austin Quach <austinquach20@gmail.edu>
2025-05-14 15:53:18 -0700

    added sample firebaseConfig file

---
+++ firebaseConfig.SAMPLE.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+const db = getFirestore(app);
+
+export {app, auth, db};
